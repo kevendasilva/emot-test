d54f971ca83c32b0e92b16dd83e0ba1c01b357bc
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2022-12-21 20:22:28 -0300

    feat: validates min and max values closes #9

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -23,11 +23,15 @@ class ComponentsController < ApplicationController
   def create
     @component = Component.new(component_params)
 
+    greater_than_or_equal = component_params[:min_value] >= component_params[:max_value]
+    component_saved = @component.save unless greater_than_or_equal
+
     respond_to do |format|
-      if @component.save
+      if component_saved
         format.html { redirect_to component_url(@component), notice: "Component was successfully created." }
         format.json { render :show, status: :created, location: @component }
       else
+        @component.errors.add :min_value, "must be less than the Max value" if greater_than_or_equal
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @component.errors, status: :unprocessable_entity }
       end
@@ -36,11 +40,15 @@ class ComponentsController < ApplicationController
 
   # PATCH/PUT /components/1 or /components/1.json
   def update
+    greater_than_or_equal = component_params[:min_value] >= component_params[:max_value]
+    component_updated = @component.update(component_params) unless greater_than_or_equal
+
     respond_to do |format|
-      if @component.update(component_params)
+      if component_updated
         format.html { redirect_to component_url(@component), notice: "Component was successfully updated." }
         format.json { render :show, status: :ok, location: @component }
       else
+        @component.errors.add :min_value, "must be less than the Max value" if greater_than_or_equal
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @component.errors, status: :unprocessable_entity }
       end
