67412ce77d988abb19179f00a54738075bbb5285
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-02-11 15:55:10 -0300

    refactor: available actuators to output

--- app/helpers/outputs_helper.rb
+++ app/helpers/outputs_helper.rb
@@ -1,6 +1,9 @@
 module OutputsHelper
-  def available_components(components)
+  def available_components()
     available = []
+
+    components = Component.where(kind: "actuator")
+
     components.each do |component|
       unless component.output
         available.append [component.name, component.id]
